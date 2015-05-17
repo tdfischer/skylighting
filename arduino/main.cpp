0ef1b9ed66d687642865dd078b0347ab94a32620
Torrie Fischer <tdfischer@hackerbots.net>
2015-05-17 12:13:45 -0700

    Fix all the color fading bugs in the twinkle anim

--- arduino/main.cpp
+++ arduino/main.cpp
@@ -12,7 +12,27 @@ SoftwareSerial dbg_ser (2, 3);
 
 #define LED_COUNT 346
 #define LED_COUNT 93
+#define LED_COUNT 28
 CRGB leds[LED_COUNT];
+
+static void fadeLEDSegment(uint16_t idx, CRGB goal, int width)
+{
+  CRGB start = leds[idx];
+  for(fract8 i = 0; i < 255; i++) {
+    CRGB blended = blend(start, goal, ease8InOutCubic(i));
+    for (uint16_t segmentIdx = idx; segmentIdx <= idx + width; segmentIdx++) {
+      leds[segmentIdx] = blended;
+    }
+    FastLED.show();
+    delay(10);
+  }
+}
+
+static void fadeLED(uint16_t idx, CRGB goal)
+{
+  return fadeLEDSegment(idx, goal, 0);
+}
+
 
 class HouselightsAnimation : public Animation {
 public:
@@ -41,12 +61,53 @@ public:
   void redraw()
   {
     uint8_t pos = idx() % LED_COUNT;
-    hue++;
+    hue = 0;
     for (uint16_t  i = 0; i < LED_COUNT; i++) {
       leds[i] = CHSV(hue, 255, 255 * ((pos - i)/(double)LED_COUNT));
     }
     FastLED.show();
     delay(30);
+  }
+};
+
+class TwinkleAnimation : public Animation {
+public:
+  TwinkleAnimation() :
+    Animation(),
+    render_func(&TwinkleAnimation::do_fill),
+    nextColor(0, 255, 255)
+  {}
+
+  void (TwinkleAnimation::*render_func)();
+  CHSV nextColor;
+  uint16_t iterations;
+
+  void redraw()
+  {
+    (this->*render_func)();
+  }
+
+  void do_fill()
+  {
+    for (uint16_t i = 0; i < LED_COUNT; i++) {
+      leds[i] = CRGB::White;
+    }
+    FastLED.show();
+    fadeLEDSegment(0, CRGB::Black, LED_COUNT);
+    nextColor = CHSV(random(0, 255), 255, 255);
+    render_func = &TwinkleAnimation::do_random_shift;
+  }
+
+  void do_random_shift()
+  {
+    uint16_t pixelIdx = random(0, LED_COUNT);
+    fadeLED(pixelIdx, nextColor);
+    iterations++;
+    if (iterations == 100) {
+      nextColor.h += random(20, 30);
+      nextColor.s = max(nextColor.s+random(10, 30), 100);
+      iterations = 0;
+    }
   }
 };
 
@@ -99,7 +160,6 @@ public:
   void do_expand() {
     width++;
     hue++;
-    hue %= 128 * 3;
     CHSV curColor = CHSV (hue, 255, max_brightness);
 
     uint16_t start, end;
@@ -136,15 +196,14 @@ public:
       pos %= LED_COUNT;
     }
     delay (10);
-    hue++;
-    hue %= 128 * 3;
   }
 };
 
 DropAnimation idleAnim;
 HouselightsAnimation houselightsAnim;
 DirectionAnimation directionAnim;
-Animation* anim (&idleAnim);
+TwinkleAnimation twinkleAnim;
+Animation* anim (&twinkleAnim);
 
 void
 do_idle(unsigned char argc, const GravitonMethodArg* argv, GravitonVariant* ret)
@@ -200,8 +259,8 @@ void setup() {
 #endif // DEBUG
 
   Serial.begin (9600);
-  //FastLED.addLeds<LPD8806, 12>(leds, LED_COUNT);
-  FastLED.addLeds<WS2812, 12, GRB>(leds, LED_COUNT);
+  FastLED.addLeds<LPD8806, GRB>(leds, LED_COUNT);
+  //FastLED.addLeds<WS2812, 12, GRB>(leds, LED_COUNT);
   randomSeed (analogRead(0));
   idleAnim.hue = random (255);
   directionAnim.hue = random (255);
